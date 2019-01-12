aae567b20c30e539e055403705a703169a81e8ce
Lex Chuthor <psc9qb@gmail.com>
2019-01-12 11:05:49 -0500

    updated schemas

---
+++ models/order.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  name: { type: String, required: true },
+  product: [{
+    type: Schema.Types.ObjectId,
+    ref: "Product"
+  }],
+  date: { type: Date, default: Date.now }
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
