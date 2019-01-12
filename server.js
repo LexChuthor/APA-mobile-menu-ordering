e318aa7028922d8567cee53028f297160a11d7d1
Alex esch <eschmassage@gmail.com>
2019-01-12 11:15:11 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
-
+const app = express()
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const passport = require("passport")
+const session = require("express-session")
+var bodyParser = require('body-parser')
+var env = require('dotenv').load()
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +19,47 @@ if (process.env.NODE_ENV === "production") {
 }
 // Add routes, both API and view
 app.use(routes);
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+app.use(express.static("public"));
+
+
+app.get('signin', function (req, res) {
+    res.send('Welcome to Passport with Sequelize');
+});
+
+
+//Models
+var models = require("./app/models");
+
+
+//Routes
+var authRoute = require('./app/routes/auth.js')(app, passport);
+
+
+//load passport strategies
+require('./app/config/passport/passport.js')(passport, models.user);
+
+// Route config -------------------------------------------/
+require("./app/routes/htmlRoutes")(app);
+require("./app/routes/apiRoutes")(app);
+
+//Sync Database
+models.sequelize.sync().then(function () {
+    console.log('Connected to Database')
+
+}).catch(function (err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
+
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
