c10f147d3ee42b5831048fb6080bd9c257242cd6
Lex Chuthor <psc9qb@gmail.com>
2019-01-15 20:26:44 -0500

    fixed conflicts

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,14 +7,9 @@ function Nav() {
         EATZ POS System
       </a>
       
-<<<<<<< HEAD
     
     <a className="navbar-brand" href="/kitchen">Kitchen Log In <img src= "http://icons-for-free.com/free-icons/png/512/1543351.png" height="30px"></img></a>
     <a className="navbar-brand" href="/linkedin"> <img src= "http://icons-for-free.com/free-icons/png/512/1543351.png" height="30px"></img></a>
-=======
-    <a class="navbar-brand" href="/signin">Kitchen Log In <img src= "http://icons-for-free.com/free-icons/png/512/1543351.png" height="30px"></img></a>
-    <a class="navbar-brand" href="/linkedin"> <img src= "https://pbs.twimg.com/profile_images/1082424539492073477/exU8rYn8_400x400.jpg" height="30px"></img></a>
->>>>>>> fed97475cee326f73c491bfe51ab99293b1fe313
     
     </nav>
   );
