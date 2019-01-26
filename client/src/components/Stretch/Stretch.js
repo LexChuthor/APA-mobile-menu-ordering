197dc37a47ebbaba9d1954bd37b5b966ede10ac9
Alex esch <eschmassage@gmail.com>
2019-01-26 12:41:24 -0500

    added stretch component to clean up backgrounds on pages! (thanks Tim!)

---
+++ client/src/components/Stretch/Stretch.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+class Stretch extends React.Component {
+  render() {
+    return (
+      <div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    );
+  }
+}
+export default Stretch;
--- client/src/view/signin.js
+++ client/src/view/signin.js
@@ -5,13 +5,13 @@ import cards from "./cards.json";
 // import signin from "../components/signin/signin";
 import kitchen from "../components/Kitchen";
 import API from "../utils/API";
+import Stretch from "../components/Stretch/Stretch";
 
 class Login extends Component {
-    state = {
-      isLoggedIn: false,
-    };
+  state = {
+    isLoggedIn: false
+  };
 
-    
   handleInputChange = event => {
     const value = event.target.value;
     const name = event.target.name;
@@ -31,17 +31,16 @@ class Login extends Component {
     console.log("state = " + JSON.stringify(this.state));
   };
 
-
   validateUser = query => {
     API.getUser(query)
       .then(res => {
         console.log("LOGIN: res = " + JSON.stringify(res));
         if (res.data.success) {
           console.log("in success handle");
-          this.setState({ isLoggedIn: true, });
+          this.setState({ isLoggedIn: true });
           this.setState({ loginMsg: res.data.message });
           window.localStorage.setItem("SMC_authkey", res.data.token);
-          window.location.assign('/kitchen');
+          window.location.assign("/kitchen");
         } else {
           console.log("in failure handle");
           this.setState({ isLoggedIn: false });
@@ -54,37 +53,54 @@ class Login extends Component {
       .catch(err => console.log(err));
   };
 
-
   render() {
-      return(
-          <Container fluid>
-            <Row>
-                <Col size="sm-12">
-                    <div id="loginForm" className="loginForm">
-                        <form id="signup" name="signup"  action="/kitchen">
-                            <Row><h1>Kitchen Log In</h1></Row>
-                            <Row>
-                                <label className="login_email" for="login_email">Email</label>
-                                <input type="email" name="email" id="login_email" placeholder="Enter your email address" onChange={this.handleInputChange}></input>
-                            </Row>
-                            <Row>
-                                <label className="login_password" for="login_password">Password</label>
-                                <input type="password" name="password" id="login_password" placeholder="Enter your password" onChange={this.handleInputChange}></input>
-                            </Row>
-                            <Row>
-                                <button className="button" onClick={this.handleFormSubmit}>Submit</button>
-                            </Row>
-                        </form>
-                    </div>
-                </Col>
-            </Row>
-          </Container>
-      )
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="sm-12">
+            <div id="loginForm" className="loginForm">
+              <form id="signup" name="signup" action="/kitchen">
+                <Row>
+                  <h1>Kitchen Log In</h1>
+                </Row>
+                <Row>
+                  <label className="login_email" for="login_email">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="login_email"
+                    placeholder="Enter your email address"
+                    onChange={this.handleInputChange}
+                  />
+                </Row>
+                <Row>
+                  <label className="login_password" for="login_password">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="login_password"
+                    placeholder="Enter your password"
+                    onChange={this.handleInputChange}
+                  />
+                </Row>
+                <Row>
+                  <button className="button" onClick={this.handleFormSubmit}>
+                    Submit
+                  </button>
+                </Row>
+              </form>
+            </div>
+          </Col>
+        </Row>
+
+        <Stretch />
+      </Container>
+    );
   }
-
-
-
 }
-
 
 export default Login;
