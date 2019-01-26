503dd4e10fddece8843b7b687633ba9153209ccd
Alex esch <eschmassage@gmail.com>
2019-01-26 13:00:06 -0500

    added stretchmini component for main page!

--- client/src/components/Stretchmini/Stretchmini.js
+++ client/src/components/Stretchmini/Stretchmini.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
-class Stretch extends React.Component {
+class Stretchmini extends React.Component {
   render() {
     return (
       <div>
@@ -14,22 +14,9 @@ class Stretch extends React.Component {
         <br />
         <br />
         <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+
       </div>
     );
   }
 }
-export default Stretch;
+export default Stretchmini;
