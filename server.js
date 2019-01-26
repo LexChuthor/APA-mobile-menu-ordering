f207c6f6b6640ce7f9bdae1b58d44aa07b38237c
Lex Chuthor <psc9qb@gmail.com>
2019-01-26 14:07:56 -0500

    heroku fixes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  });
+
   // app.get('/^\/(?!api.*/', (req, res) => {
   //   res.sendFile(path.join(__dirname, './client/public/index.html'));
   // });
