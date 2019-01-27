1f4e019e4615621a2a2ace465ccc8b2daaac38f6
Lex Chuthor <psc9qb@gmail.com>
2019-01-27 15:12:24 -0500

    console log

--- client/src/view/kitchen.js
+++ client/src/view/kitchen.js
@@ -73,6 +73,7 @@ class Kitchen extends Component {
           <Col size="sm-12">
             {/*  Jumbotron */}
             <Wrapper>
+              {console.log(this.state.orders)}
               {this.state.orders.map((order, i) => (
                 order.completed === false &&
                 <OrderCard
